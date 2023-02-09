db7ffcb86d043758d7aff31ec57da79110d3e4f7
carlosestevesjr <carlosestevesjr0@gmail.com>
2023-02-08 23:32:13 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
+/* eslint-disable react/style-prop-object */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { Provider, useSelector } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store/index';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({navigation}) => {
+    const teste = useSelector((state) => state);
+    console.log(teste)
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Profile', {name: 'Jane'})
+        }
+      />
+    );
+};
+const ProfileScreen = ({navigation, route}) => {
+    return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+const MyStack = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{title: 'Welcome'}}
+          />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+};
 
 export default function App() {
+
+  useEffect(() => {
+    console.log('teste')
+  },
+  []);
+//   const teste = useSelector((state) => state);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <>
+      <StatusBar style="dark" />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+            <MyStack></MyStack>
+        </PersistGate>
+      </Provider>
+   </>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
